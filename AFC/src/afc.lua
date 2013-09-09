c947489bb96c6f56a7007a35d716dc855e6e943b
Sillywolf <ustcsxl@gmail.com>
2013-09-09 14:36:51 +0800

    加入memcPool支持

---
+++ AFC/src/afc.lua
@@ -0,0 +1,42 @@
+ngx.req.read_body()
+local user_agent = ngx.var.http_user_agent;
+local cookie = ngx.req.get_headers()["Cookie"];
+local accessip = ngx.var.remote_addr;
+local uri = ngx.unescape_uri(ngx.var.request_uri);
+local username = getRequestParam("username");
+
+if cookie == nil then
+    --print("cookie is nil");
+    cookie = "";
+end
+if user_agent == nil then
+    --print("ua is nil");
+    user_agent = "";
+end
+if username == nil then
+    --print("ua is nil");
+    username = "";
+end
+
+print("uri:"..uri.."/username:"..username.."/user_agent:"..user_agent.."/cookie:"..cookie.."/accessip:"..accessip);
+
+if ngx.re.match(ngx.unescape_uri(cookie),"access=deny","isjo")then
+    print("cookie deny");
+    serviceDeny();
+end
+
+
+if not validStaticRules(user_agent,"user_agent") then
+    print("ua deny");
+    serviceDeny();
+end
+
+if not validDynamicRule("accessip:"..accessip) then
+    print("ip deny");
+    serviceDeny();
+end
+
+if uri == "/" and not validDynamicRule("loginip:"..accessip) then
+    print("loginip deny");
+    serviceDeny();
+end
